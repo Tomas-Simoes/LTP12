fea3cd4bc9821b2a85ac9c7b0422db5fa2ad4089
Tomas-Simoes <tomas20simoes@gmail.com>
2022-10-07 12:32:45 +0000

    App 2 completed

--- ASP.NET CORE/App2/Controllers/HomeController.cs
+++ ASP.NET CORE/App2/Controllers/HomeController.cs
@@ -6,15 +6,28 @@ namespace App2.Controllers
 {
     public class HomeController : Controller
     {
-
         public IActionResult Index()
         {
             return View("Inicio");
         }
 
+        public ViewResult Inscritos()
+        {
+            return View("ListaFinal", Repositorio.ListaFinal.Where(c=>c.Inscrever==true));
+        }
+
+        [HttpGet]
         public ViewResult Formulario()
         {
             return View("Formulario");
+        }
+
+        [HttpPost] 
+        public ViewResult Formulario(RespostaInscricao resposta)
+        {
+            // adicionar a lista
+            Repositorio.AdicionarResposta(resposta);
+            return View("Obrigado", resposta);
         }
     }
 }
---
+++ ASP.NET CORE/App2/Models/Repositorio.cs
@@ -0,0 +1,17 @@
+﻿namespace App2.Models
+{
+    public class Repositorio
+    {
+        private static List<RespostaInscricao> LISTA = new List<RespostaInscricao>();   
+        
+        public static void AdicionarResposta(RespostaInscricao resposta)
+        {
+            LISTA.Add(resposta);
+        }
+
+        public static IEnumerable<RespostaInscricao> ListaFinal
+        {
+            get { return LISTA; }
+        }
+    }
+}
