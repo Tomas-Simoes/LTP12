a4b2fcb63254085578b1f1c411cfed7db6f00176
Tomas-Simoes <tomas20simoes@gmail.com>
2022-10-12 16:30:08 +0000

    .

---
+++ ASP.NET CORE/AppFicha5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppFicha5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
