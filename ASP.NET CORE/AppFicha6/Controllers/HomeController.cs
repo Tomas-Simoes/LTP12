12bcdbfc7cf01eb1058234fb8f0ddc21337c49df
Tomas-Simoes <tomas20simoes@gmail.com>
2022-10-14 12:33:53 +0000

    Ficha 7 done

---
+++ ASP.NET CORE/AppFicha6/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using AppFicha6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AppFicha6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("View");
+        }
+    }
+}
---
+++ ASP.NET CORE/AppFicha7/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using AppFicha7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Diagnostics;
+
+namespace AppFicha7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        
+        public IActionResult About()
+        {
+            return View("About");
+        }
+        
+        public IActionResult Contact()
+        {
+            return View("Contact");
+        }
+    }
+}
