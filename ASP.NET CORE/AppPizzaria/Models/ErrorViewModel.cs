590e13884cee6c0e8fca15793726851c479ba20d
Tomas-Simoes <tomas20simoes@gmail.com>
2022-10-28 12:33:40 +0000

    -

---
+++ ASP.NET CORE/AppPizzaria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppPizzaria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
