960ad4fc8955753a3980d6fb2c7c1a86f635ce54
Tomas-Simoes <tomas20simoes@gmail.com>
2022-11-02 16:38:49 +0000

    .

---
+++ ASP.NET CORE/AppPizzaria/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace AppPizzaria.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ ASP.NET CORE/AppPizzaria/Models/Pizza.cs
@@ -0,0 +1,17 @@
+﻿namespace AppPizzaria.Models
+{
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsPizzaPreferida { get; set; }
+        public bool EmStock { get; set; }
+        public int CategoriaId { get; set; }
+
+    }
+}
