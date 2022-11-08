c29c64d6f259144f5bd4adfbd0ce0b716ec2541f
Tomas-Simoes <tomas20simoes@gmail.com>
2022-11-08 11:51:20 +0000

    .

--- ASP.NET CORE/AppPizzaria/Models/Categoria.cs
+++ ASP.NET CORE/AppPizzaria/Models/Categoria.cs
@@ -1,8 +1,16 @@
-﻿namespace AppPizzaria.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppPizzaria.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Insira o nome da categoria")]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public List<Pizza> Pizzas { get; set; }
